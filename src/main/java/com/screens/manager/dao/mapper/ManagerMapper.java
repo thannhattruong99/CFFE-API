3e2cc46166eeb42661e486a7e7c64b6730150ab5
thannhattruong99 <truongtnse130426@fpt.edu.vn>
2021-05-25 18:31:29 +0700

    [TruongTN] Update reason inactive.

--- src/main/java/com/screens/manager/dao/mapper/ManagerMapper.java
+++ src/main/java/com/screens/manager/dao/mapper/ManagerMapper.java
@@ -2,7 +2,6 @@ package com.screens.manager.dao.mapper;
 
 import com.common.dao.BaseDAO;
 import com.screens.manager.dto.ManagerDTO;
-import com.screens.city.form.CityResponseSupporter;
 import com.screens.manager.form.ResponseManagerDetailForm;
 import com.screens.manager.form.ResponseManagerListForm;
 import com.util.IDBHelper;
