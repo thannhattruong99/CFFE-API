169ed6f7879b108a85c46cbb229ae2e74cfd8da8
thannhattruong99 <truongtnse130426@fpt.edu.vn>
2021-06-02 16:49:11 +0700

    [TruongTN] update available camera list

--- src/main/java/com/screens/camera/form/RequestAvailableCameraListForm.java
+++ src/main/java/com/screens/camera/form/RequestAvailableCameraListForm.java
@@ -4,10 +4,38 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
 public class RequestAvailableCameraListForm {
+    private String cameraName;
+    private int pageNum;
+    private int fetchNext;
     @Min(value = 1, message = "MSG-009") @Max(value = 2, message = "MSG-009")
     private int typeDetect;
 
+
     public RequestAvailableCameraListForm() {
+    }
+
+    public String getCameraName() {
+        return cameraName;
+    }
+
+    public void setCameraName(String cameraName) {
+        this.cameraName = cameraName;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getFetchNext() {
+        return fetchNext;
+    }
+
+    public void setFetchNext(int fetchNext) {
+        this.fetchNext = fetchNext;
     }
 
     public int getTypeDetect() {
