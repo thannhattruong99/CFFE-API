b23d9b20ab26796670851285236b7b78b588c8f6
NhatTruong <truongtnse130426@fpt.edu.vn>
2021-10-19 14:49:49 +0700

    [TruongTN] Add deploy url

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 CFFE - Cucstomer feedback through facial expression analysis.
-This repositoyr is based on CapstoneAPI.
+This repository is checkout from CapstoneAPI.
+CFFE is using Spring Boot, Spring Security, MyBatis, JWT, Swagger
+SwaggerUI: https://cffe-api.herokuapp.com/swagger-ui/index.html?configUrl=/api-docs/swagger-config
--- src/main/java/com/util/DBHelper.java
+++ src/main/java/com/util/DBHelper.java
@@ -5,12 +5,13 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.Reader;
 
-@Service
+@Component
 public class DBHelper implements IDBHelper{
     private static final Logger logger = LoggerFactory.getLogger(DBHelper.class);
     @Override
